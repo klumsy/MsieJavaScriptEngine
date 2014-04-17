6480c4bdfe5c4caab7009c49ab30e2f48e2ab0f4
Karl Prosser <klumsy@gmail.com>
2014-04-16 20:57:15 -0700

    removing everything because moving project to PowerShellJS

--- ChakraHost/MsieJavaScriptEngine.Tests/Resources/square.js
+++
@@ -1,3 +0,0 @@
-﻿function square(num) {
-	return num * num;
-}
--- ChakraHost/NuGet/build-package.cmd
+++
@@ -1,2 +0,0 @@
-\Windows\Microsoft.NET\Framework\v4.0.30319\msbuild.exe ..\MsieJavaScriptEngine\MsieJavaScriptEngine.csproj /p:Configuration=Release
-nuget.exe pack MsieJavaScriptEngine.nuspec
